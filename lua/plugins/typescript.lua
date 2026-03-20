f5b7d2adaedc610d547e8534777b3e60d111b44e
renben-dev <bidone.renzo@gmail.com>
2026-03-20 15:09:31 +0100

    test commit

--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -1,10 +1,10 @@
-return {{
-    "neovim/nvim-lspconfig",
-    config = function()
-        local lspconfig = require("lspconfig")
-        -- Mason-installed servers are automatically detected
-        lspconfig.vtsls.setup({})
-        lspconfig.eslint.setup({})
-    end
-} :w
+return { {
+  "neovim/nvim-lspconfig",
+  config = function()
+    local lspconfig = require("lspconfig")
+    -- Mason-installed servers are automatically detected
+    lspconfig.vtsls.setup({})
+    lspconfig.eslint.setup({})
+  end
+}
 }
